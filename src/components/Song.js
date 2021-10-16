74e22d7c562fb211833d6a1e66c733539e35692b
sebaszwenger <sebaszwenger@gmail.com>
2021-10-16 20:25:57 -0300

    create new component Song to show the lyric of the song

---
+++ src/components/Song.js
@@ -0,0 +1,11 @@
+const Song = ({ lyric }) => {
+  if (lyric.length === 0) return null;
+  return (
+    <>
+      <h2>Letra Canción</h2>
+      <p className="letra">{lyric}</p>
+    </>
+  );
+};
+
+export default Song;
